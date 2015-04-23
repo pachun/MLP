94ed4a16adcc6140456a41afbf8e08b311b026a2
Nicholas Pachulski <nick@pachulski.me>
2015-04-22 23:28:34 -0400

    adapt stock spec for iOS 8 compatibility

--- spec/main_spec.rb
+++ spec/main_spec.rb
@@ -3,7 +3,9 @@ describe "Application 'mlp'" do
     @app = UIApplication.sharedApplication
   end
 
-  it "has one window" do
-    @app.windows.size.should == 1
+  it "has one window and one (new in iOS 8) overlayed window for specs" do
+    @app.windows.size.should == 2
+    @app.windows.map(&:class).should.include(UIWindow)
+    @app.windows.map(&:class).should.include(UITextEffectsWindow)
   end
 end
