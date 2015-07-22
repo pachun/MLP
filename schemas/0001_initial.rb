af98d0a3001e64b5fa61a7146bdd5d6ff03b43b0
Nick Pachulski <nick@pachulski.me>
2015-07-21 20:15:50 -0400

    Delete commented out example schema.

--- schemas/0001_initial.rb
+++ schemas/0001_initial.rb
@@ -14,26 +14,4 @@ schema "0001 initial" do
     datetime :created_at, default: Time.now, optional: false
     datetime :updated_at, default: Time.now, optional: false
   end
-
-  # Examples:
-  #
-  # entity "Person" do
-  #   string :name, optional: false
-  #
-  #   has_many :posts
-  # end
-  #
-  # entity "Post" do
-  #   string :title, optional: false
-  #   string :body
-  #
-  #   datetime :created_at
-  #   datetime :updated_at
-  #
-  #   has_many :replies, inverse: "Post.parent"
-  #   belongs_to :parent, inverse: "Post.replies"
-  #
-  #   belongs_to :person
-  # end
-
 end
